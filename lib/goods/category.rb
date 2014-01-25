3b2a351ddc3c219888f075d8074d09573a2b24ca
Artem Pyanykh <artem.pyanykh@gmail.com>
2014-01-25 12:54:27 +0400

    Add graph traversing methods for Goods::Category.
    The following methods were added:
    
    * level
    * root
    * parent_at_level
    
    It should help with 'tree prunning'.

--- lib/goods/category.rb
+++ lib/goods/category.rb
@@ -7,9 +7,40 @@ module Goods
 
     def initialize(description)
       self.description = description
+      @parent_at_level = {}
+    end
+
+    def level
+      @level ||=
+        begin
+          if parent
+            parent.level + 1
+          else
+            0
+          end
+        end
+    end
+
+    def root
+      return self unless parent
+      @root ||= parent.root
+    end
+
+    def parent_at_level(level)
+      valid_parent_level?(level) or raise ArgumentError.new('incorrect level')
+
+      if self.level == level
+        self
+      else
+        @parent_at_level[level] ||= parent.parent_at_level(level)
+      end
     end
 
     private
+
+    def valid_parent_level?(level)
+      level >= 0 && level <= self.level
+    end
 
     def apply_validation_rules
       validate :id, proc { |val| !(val.nil? || val.empty?) }
--- spec/goods/category_spec.rb
+++ spec/goods/category_spec.rb
@@ -2,36 +2,89 @@ require "spec_helper"
 
 describe Goods::Category do
   let(:valid_description) { {id: "1", name: "Name"} }
+  let(:root) { Goods::Category.new valid_description }
 
   it_should_behave_like "containable" do
     let(:element) { Goods::Category.new(valid_description) }
   end
 
   describe "#valid?" do
-    let(:parent) { Goods::Category.new(id: "1", name: "Parent") }
-
-    it "should reject categories with no parent when one should be" do
+    it "should reject categories with no root when one should be" do
       invalid_child = Goods::Category.new(id: "3", name: "Invalid child", parent_id: "2")
       expect(invalid_child).not_to be_valid
     end
 
     it "should reject categories with incorrect parrent" do
       invalid_child = Goods::Category.new(id: "3", name: "Invalid child", parent_id: "2")
-      invalid_child.parent = parent
+      invalid_child.parent = root
       expect(invalid_child).not_to be_valid
     end
 
-    it "should accept categories with no parent" do
+    it "should accept categories with no root" do
       child = Goods::Category.new(valid_description)
       child.valid?
-      puts child.invalid_fields
       expect(child).to be_valid
     end
 
-    it "should accept categories with correct parent" do
+    it "should accept categories with correct root" do
       valid_child = Goods::Category.new(id: "3", name: "Invalid child", parent_id: "1")
-      valid_child.parent = parent
+      valid_child.parent = root
       expect(valid_child).to be_valid
     end
   end
+
+  context "on graph properties" do
+    let(:first_level) do
+      c = Goods::Category.new valid_description.merge parent_id: root.id
+      c.parent = root
+      c
+    end
+    let(:second_level) do
+      c = Goods::Category.new valid_description.merge parent_id: first_level.id
+      c.parent = first_level
+      c
+    end
+
+    describe "#level" do
+      it "should be 0 for root nodes" do
+        expect(root.level).to eq(0)
+      end
+
+      it "should be parent level + for for child nodes" do
+        expect(first_level.level).to eq(root.level + 1)
+      end
+    end
+
+    describe "#root" do
+      it "should be root itself if called on root" do
+        expect(root.root).to be(root)
+      end
+
+      it "should be root of a branch if called on child" do
+        expect(second_level.root).to be(root)
+      end
+    end
+
+    describe "#parent_at_level" do
+
+      it "should raise error if level < 0" do
+        expect { root.parent_at_level(-1) }.to raise_error(ArgumentError)
+      end
+
+      it "should return self if called for object's level" do
+        expect(first_level.parent_at_level(first_level.level)).to be(first_level)
+      end
+
+      it "should raise error if level > object level" do
+        expect { first_level.parent_at_level first_level.level + 1 }.
+          to raise_error(ArgumentError)
+      end
+
+      it "should return parent at specified level from the 'root'" do
+        expect(second_level.parent_at_level(1)).to be(first_level)
+        expect(second_level.parent_at_level(0)).to be(root)
+      end
+    end
+  end
+
 end
